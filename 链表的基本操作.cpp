a1e53b9783487aec9451210d73568a082eee184a
liuyvxin52 <xinxintpai@outlook.com>
2023-09-15 22:09:28 +0800

    new file:   .vscode/settings.json 	new file:   "\351\223\276\350\241\250\347\232\204\345\237\272\346\234\254\346\223\215\344\275\234.cpp"

---
+++ 链表的基本操作.cpp
@@ -0,0 +1,63 @@
+/*
+插入的实现:
+e -> prev = p;
+e -> next = p -> next;
+p -> next -> prev = e;
+p -> nexxt = e;
+*/
+/*
+删除的实现:
+p2 -> next = p2 -> next -> next;
+p3 -> next -> prev = p2;
+delete p3;
+*/
+#include<bits/stdc++.h>
+
+using namespace std;
+
+//节点定义
+struct Node{
+    int data;
+    struct Node *next;
+};
+
+//头节点
+Node *head = NULL;
+
+//在链表末尾追加
+void add(int x){
+    //如果有head的值
+    if(head != NULL){
+        Node *a = new Node;
+        a -> data = x;
+        a -> next = NULL;
+        Node *p = head;
+        //移动到最后一个节点
+        while(p -> next != NULL){
+            p = p -> next;
+        }
+        p -> next = a;
+    }
+    else{
+        head = new Node;
+        head -> data = x;
+        head -> next = NULL;
+    }
+}
+
+//在中间追加元素
+void insert(int n, int x){
+    //新节点
+    Node *d = new Node;
+    d -> data = x;
+    d -> next = NULL;
+    //如果是头节点
+    if(n == 1){
+        d -> next = head;
+        head = d;
+    }
+    else{
+        int i;
+        Node *p
+    }
+}
